1115fa103348607425e073cd3a8058788a8df221
Vincent Schoettke <vincentschoettke@gmail.com>
2014-08-16 22:26:08 +0200

    Added download of required files

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 alias unzip-stream="python -c \"import zipfile,sys,StringIO;zipfile.ZipFile(StringIO.StringIO(sys.stdin.read())).extractall(sys.argv[1] if len(sys.argv) == 2 else '.')\""
 
-# curl https://raw.githubusercontent.com/davidgaya/docker-apache-php-oci/master/instantclient-basic-linux.x64-12.1.0.2.0.zip  | unzip-stream ./
-# curl https://raw.githubusercontent.com/davidgaya/docker-apache-php-oci/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip | unzip-stream ./
+curl https://raw.githubusercontent.com/davidgaya/docker-apache-php-oci/master/instantclient-basic-linux.x64-12.1.0.2.0.zip  | unzip-stream ./
+curl https://raw.githubusercontent.com/davidgaya/docker-apache-php-oci/master/instantclient-sdk-linux.x64-12.1.0.2.0.zip | unzip-stream ./
 #wget https://raw.githubusercontent.com/davidgaya/docker-apache-php-oci/master/instantclient-sqlplus-linux.x64-12.1.0.2.0.zip -O - | unzip-stream ./
 
 pushd .
